9542db5e9d39adc7f16ceb946af432df1306ab93
Christian Aliaga <chaliaga99@gmail.com>
2022-02-21 01:04:52 -0500

    Refactoring and add dependencies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {DataStore} from "aws-amplify";
+import {Todo} from "../models";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,64 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'to-do-app';
+  models: Todo[] | undefined;
+  taskTitle = '';
+  taskDescription: string | undefined = '';
+  id = '';
+  currentAction = 'Add New';
+  taskToUpdate: Todo | undefined;
+  totalTask = '';
+
+  async ngOnInit() {
+    await this.getTask();
+  }
+
+  async addTask() {
+    if (this.taskTitle && this.taskDescription) {
+      if (!this.id) {
+        await DataStore.save(
+          new Todo({
+            "name": this.taskTitle,
+            "description": this.taskDescription
+          })
+        );
+      } else {
+        /* Models in DataStore are immutable. To update a record you must use the copyOf function
+ to apply updates to the item’s fields rather than mutating the instance directly */
+        await DataStore.save(Todo.copyOf(this.taskToUpdate, item => {
+          item.name = this.taskTitle;
+          item.description = this.taskDescription;
+        }));
+      }
+
+      this.cleanForm();
+      await this.getTask();
+    }
+  }
+
+  async getTask() {
+    this.models = await DataStore.query(Todo);
+    this.totalTask = this.models.length.toString() + " Tasks for today";
+  }
+
+  async deleteTask(id: string) {
+    const taskToDelete = <Todo>await DataStore.query(Todo, id);
+    await DataStore.delete(taskToDelete);
+    await this.getTask();
+  }
+
+  cleanForm() {
+    this.id = '';
+    this.taskTitle = '';
+    this.taskDescription = '';
+    this.currentAction = 'Add New';
+  }
+
+  async updateTask(id: string) {
+    this.currentAction = 'Edit Task';
+    this.taskToUpdate = <Todo> await DataStore.query(Todo, id);
+    this.id = this.taskToUpdate.id;
+    this.taskTitle = this.taskToUpdate.name;
+    this.taskDescription = this.taskToUpdate.description;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {FormsModule} from "@angular/forms";
+import {ButtonModule} from "primeng/button";
+import {InputTextModule} from "primeng/inputtext";
+import {InputTextareaModule} from "primeng/inputtextarea";
+import {SplitterModule} from "primeng/splitter";
+import {ChipModule} from "primeng/chip";
 
 @NgModule({
   declarations: [
@@ -10,7 +16,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ButtonModule,
+    InputTextModule,
+    InputTextareaModule,
+    SplitterModule,
+    ChipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
